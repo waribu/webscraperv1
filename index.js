34634fd3aca970712d59d7aedd604a3a6db4b762
Waribu George <gwaribu@gmail.com>
2022-08-20 13:00:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const axios = require ('axios')
+const cheerio = require ('cheerio')
+const express = require ('express')
+const PORT = 8000
+
+const app = express ()
+
+const url = 'https://www.theguardian.com/uk/sport'
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+
+        $('.fc-item__title', html).each(function() {
+            const title =$(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+        })
+        console.log(articles)
+    }).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT} `) )
+
